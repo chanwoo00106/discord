12085c7a41aca7222caac454d7e33346c8abfac7
변찬우 <chan00106@naver.com>
2022-03-04 08:30:20 +0900

    Update 버그 수정

--- discord.ts/src/commands/meal.ts
+++ discord.ts/src/commands/meal.ts
@@ -2,7 +2,7 @@ import { CommandInteraction, Message, MessageEmbed } from "discord.js";
 import { Discord, Slash } from "discordx";
 import { MealType } from "../types/mealType";
 import axios from "axios";
-
+const meal = ["아침", "점심", "저녁"];
 @Discord()
 class MealDiscord {
   @Slash("meal")
@@ -18,34 +18,21 @@ class MealDiscord {
       process.env.MEAL_API + `${date.getFullYear()}${month}${day}`;
     try {
       const { data }: { data: MealType } = await axios.get(queryUrl);
+
+      if (!data.mealServiceDietInfo) {
+        const embeds = new MessageEmbed().setTitle("급식이 없는 날입니다");
+        interaction.reply({ embeds: [embeds] });
+      }
+
       const re: RegExp = /<br\/>/gi;
       const embeds = new MessageEmbed()
         .setTitle(`${date.getFullYear()}년 ${month}윌 ${day}일 급식`)
         .addFields(
-          {
-            name: "아침",
-            value: data.mealServiceDietInfo[1].row[0].DDISH_NM.replace(
-              re,
-              "\n"
-            ),
+          data.mealServiceDietInfo[1].row.map((dishName, i) => ({
+            name: meal[i],
+            value: dishName.DDISH_NM.replace(re, "\n"),
             inline: true,
-          },
-          {
-            name: "점심",
-            value: data.mealServiceDietInfo[1].row[1].DDISH_NM.replace(
-              re,
-              "\n"
-            ),
-            inline: true,
-          },
-          {
-            name: "저녁",
-            value: data.mealServiceDietInfo[1].row[2].DDISH_NM.replace(
-              re,
-              "\n"
-            ),
-            inline: true,
-          }
+          }))
         )
         .setFooter({
           text: interaction.member?.user.username || "인식하지 못했어요.",
@@ -57,6 +44,7 @@ class MealDiscord {
 
       interaction.reply({ embeds: [embeds] });
     } catch (e) {
+      console.log(e);
       const embeds = new MessageEmbed().setTitle("오류가 생겼습니다.");
       interaction.reply({ embeds: [embeds] });
     }
