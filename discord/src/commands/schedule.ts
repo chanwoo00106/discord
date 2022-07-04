d4465a615c0c53c0e4205081e29f10fe159d385c
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-07-04 09:58:02 +0900

    Update schedule

--- discord/src/commands/schedule.ts
+++ discord/src/commands/schedule.ts
@@ -26,34 +26,27 @@ export const data = new SlashCommandBuilder()
   );
 
 export async function execute(interaction: CommandInteraction) {
-  await interaction.reply({ embeds: [LoadingEmbed(interaction)] });
-
   const atDate = interaction.options.getString("week") as Week;
 
   const [date, month, day] = dateCalc(atDate);
 
-  try {
-    const { data } = await axios.get<ScheduleType>(
-      `${config.SCHEDULE_API}${date.getFullYear()}${month}${day}`
-    );
+  const { data } = await axios.get<ScheduleType>(
+    `${config.SCHEDULE_API}${date.getFullYear()}${month}${day}`
+  );
 
-    if (!data.hisTimetable.length) throw new Error();
+  if (!data.hisTimetable.length) throw new Error();
 
-    const result = data.hisTimetable[1].row?.map((i) => i.ITRT_CNTNT);
+  const result = data.hisTimetable[1].row?.map((i) => i.ITRT_CNTNT);
 
-    const embeds = new MessageEmbed()
-      .setTitle(`${date.getFullYear()}년 ${month}윌 ${day}일 시간표`)
-      .setFields(
-        ...result?.map((i, index) => ({ name: `${index + 1}교시`, value: i }))
-      )
-      .setColor("#1dd1a1")
-      .setFooter({
-        text: interaction.member?.user.username || "인식하지 못했어요.",
-        iconURL: `https://cdn.discordapp.com/avatars/${interaction.member?.user.id}/${interaction.member?.user.avatar}.png`,
-      })
-      .setTimestamp();
-    return interaction.editReply({ embeds: [embeds] });
-  } catch (e) {
-    return interaction.editReply({ embeds: [errorEmbed(interaction)] });
-  }
+  return new MessageEmbed()
+    .setTitle(`${date.getFullYear()}년 ${month}윌 ${day}일 시간표`)
+    .setFields(
+      ...result?.map((i, index) => ({ name: `${index + 1}교시`, value: i }))
+    )
+    .setColor("#1dd1a1")
+    .setFooter({
+      text: interaction.member?.user.username || "인식하지 못했어요.",
+      iconURL: `https://cdn.discordapp.com/avatars/${interaction.member?.user.id}/${interaction.member?.user.avatar}.png`,
+    })
+    .setTimestamp();
 }
